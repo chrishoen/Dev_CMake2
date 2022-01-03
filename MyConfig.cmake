dcfc4eb075620a63d77a694678e0a1df5e94b3f6
chrishoen <chris123hoen@gmail.com>
2022-01-03 16:00:07 -0500

    works for ubuntu

--- MyConfig.cmake
+++ MyConfig.cmake
@@ -1,18 +1,19 @@
 #*******************************************************************************
 #*******************************************************************************
 #*******************************************************************************
-# copied from MyConfig_win.cmake
+# copied from MyConfig_ubuntu.cmake
 
 function(my_init_config_variables)
 
-   set (MyTargetType windows PARENT_SCOPE)
+   set (MyTargetType ubuntu PARENT_SCOPE)
 
+   set (MyTargetHostName "main@cpdev1" PARENT_SCOPE)
    set (MyTargetHostName "127.0.0.1" PARENT_SCOPE)
 
-   set (MyCopyBinToDir "C:/aaa_prime/CMake" PARENT_SCOPE)
-   set (MyCopyLibToDir "C:/MyTools/MyLib" PARENT_SCOPE)
+   set (MyCopyBinToDir "/opt/prime/bin" PARENT_SCOPE)
+   set (MyCopyLibToDir "/opt/prime/lib" PARENT_SCOPE)
 
-   set (MySudo "" PARENT_SCOPE)
+   set (MySudo "sudo " PARENT_SCOPE)
 
 endfunction()
 
