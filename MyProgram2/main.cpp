72fe61faa8029a54628dca6c3bb6ac4551702b64
chrishoen <chris123hoen@gmail.com>
2022-01-03 15:41:31 -0500

    removed MyProgam3

--- MyProgram2/main.cpp
+++ MyProgram2/main.cpp
@@ -1,9 +1,11 @@
 #include "stdafx.h"
 #include "MyFunction1.h"
+#include "MyFunction2.h"
 
 int main(int argc, char** argv)
 {
    printf("Hello World - MyProgram2\n");
    doFunction1();
+   doFunction2();
    return 0;
 }
--- MyProgram3/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "stdafx.h"
-#include "MyFunction2.h"
-
-int main(int argc, char** argv)
-{
-   printf("Hello World - MyProgram3\n");
-   doFunction2();
-   return 0;
-}
